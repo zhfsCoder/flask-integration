8d9e4f84528bf3f357bb4cc62ebd540b32e7d2b8
zhfscoder <zhfscoder@hotmial.com>
2017-04-05 16:44:36 +0800

    add upload file

--- app/main/form.py
+++ app/main/form.py
@@ -1,7 +1,13 @@
+from flask_wtf.file import FileField
 from flask_wtf import Form
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField, PasswordField, SelectField, validators
 from wtforms.validators import Required
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
+    username = StringField('username', [validators.Length(min=4, max=25)])
+    emain    = StringField('Email Address', [validators.Length(min=6, max=35)])
     submit = SubmitField('Subit')
+
+class PhotoForm(Form):
+    photo = FileField('Your Photo')
--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,23 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, session, redirect
 from . import main
-from .form import NameForm
+from .form import NameForm, PhotoForm
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('.index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
+@main.route('/upload', methods=['GET', 'POST'])
+def upload():
+    form = PhotoForm()
+    if form.validate_on_submit():
+        filename = secure_filename(form.photo.data.filenam)
+    else:
+        filename = None
+    return render_template('upload.html', form=form, filename=filename)
 
 @main.route('/user/<name>')
 def user(name):
