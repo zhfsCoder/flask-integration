4d6a80a1060146aec641d8fad4eac24bd39bfd72
zhfscoder <zhfscoder@hotmial.com>
2017-04-05 10:56:29 +0800

    add the wtf(flask-form)

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,15 @@
 from flask import render_template, url_for
 from . import main
+from .form import NameForm
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 @main.route('/user/<name>')
 def user(name):
