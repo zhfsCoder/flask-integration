cf6d5378734a7f9e4b46db5a15b8e2a52a453f2b
zhfscoder <zhfscoder@hotmial.com>
2017-04-05 09:58:27 +0800

    add requrements.txt and user.html

--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,7 @@ from . import main
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
