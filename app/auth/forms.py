b7ecd84a1436e8999f072f8d4f959892fd805d56
zhfscoder <zhfscoder@hotmial.com>
2017-04-11 11:08:26 +0800

    add the register

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,30 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
 class LoginForm(Form):
     email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
     password = PasswordField('Password', validators=[Required()])
     remeber_me = BooleanField('keep me logged in')
     submit = SubmitField('Log In')
+    
+class RegistrationForm(Form):
+    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
+    username = StringField('Username', validators=[Required(), Length(1, 64), 
+                          Regexp('^[A-Za-z][A-za-z0-9_.]*$', 0, 
+                          'Username must have only letters,'
+                          'numbers, dots or uderscores')])
+    password = PasswordField('Password', validators=[
+                           Required(), EqualTo('password2', message='Password must match.')])
+    password2 = PasswordField('Confirm password', validators=[Required()])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+    
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username alredy in use.')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,21 @@
 from flask import render_template, redirect, request, url_for, flash
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from flask_login import login_user, logout_user, login_required
 from ..models import User
+from .. import db
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                   username=form.username.data,
+                   password=form.password.data)
+        db.session.add(user)
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
